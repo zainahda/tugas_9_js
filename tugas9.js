be705a054fe72bed5f4100878bb783757153d867
zainahda <zainahda@gmail.com>
2022-02-03 14:12:55 +0700

    Niomic tugas 9 Js

---
+++ tugas9.js
@@ -0,0 +1,11 @@
+let dataMahasiswa = {
+  namaDepan: "Zain",
+  namaBelakang: "Ahda",
+  jurusan: "Bahasa dan Sastra Arab",
+  gender: "Laki-Laki",
+  umur: 32,
+}
+
+for (let x in dataMahasiswa) {
+  console.log(dataMahasiswa[x]);
+}
